46f3805c90093f0d261aadab36c6ac537af9e77c
dlehdwn <155503289+dlehdwn@users.noreply.github.com>
2024-01-03 12:50:33 +0900

    Add files via upload

---
+++ day01/output_input.py
@@ -0,0 +1,19 @@
+# 메모용/주석문 [코드 실행 안됨]
+print("이동주 : 미래에 도움이 되지 않을까??")
+
+# f문자열문법 :  print(f"") 연산이 들어가는 경우 {} 사용
+print(f"123+456+789 = {123 + 456 + 789}")
+
+# 변수 : 값을 담는 상자
+
+# name 변수 사용
+# = (넣는다)
+name = "이동주"
+mbti = "isfp"
+print(name)
+print(mbti)
+print(f"저의 이름은 {name}이고 mbti는 {mbti}입니다")
+
+# input() [유저한테 입력값을 받는 기능]
+your_name = input("당신의 이름은:")
+print(f"당신의 이름은 {your_name}이군요!!")
